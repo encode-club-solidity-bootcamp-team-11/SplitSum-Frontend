9378eb027c2e013e4c6f2172746690aebe875d51
Samnang Chhun <samnang.chhun@gmail.com>
2022-08-30 22:56:00 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'split_sum_front_end';
+}
